3b94c350947615d123cf834aad09779930f6fb38
Max Persson <max@looplab.se>
2014-11-20 09:52:51 +0100

    Add HasKey checker for maps and cleanup Contains checker

--- testing/checkers.go
+++ testing/checkers.go
@@ -15,37 +15,72 @@
 package testing
 
 import (
+	"reflect"
+
 	"gopkg.in/check.v1"
 )
 
 // Contains is a checker for gocheck that asserts if an item is in a slice.
-var Contains = &containsChecker{
-	&check.CheckerInfo{
-		Name: "Contains",
-		Params: []string{
-			"Container",
-			"Expected to contain",
-		},
-	},
-}
+var Contains = &contains{}
 
-type containsChecker struct {
-	*check.CheckerInfo
-}
+// HasKey is a checker for gocheck that asserts if a map has a key.
+var HasKey = &hasKey{}
 
-func (c *containsChecker) Check(params []interface{}, names []string) (result bool, error string) {
+type contains struct{}
+
+func (c *contains) Check(params []interface{}, names []string) (bool, string) {
+	if len(params) != 2 {
+		return false, "Contains takes 2 arguments: a slice and an item"
+	}
+
 	slice, ok := params[0].([]interface{})
 	if !ok {
-		return false, "First parameter is not a []interface{}"
+		return false, "first parameter is not a []interface{}"
 	}
 	value, ok := params[1].(interface{})
 	if !ok {
-		return false, "Second parameter is not an int"
+		return false, "second parameter is not an interface"
 	}
+
 	for _, v := range slice {
 		if v == value {
 			return true, ""
 		}
 	}
+
 	return false, ""
+}
+
+func (c *contains) Info() *check.CheckerInfo {
+	return &check.CheckerInfo{
+		Name:   "Contains",
+		Params: []string{"slice", "item"},
+	}
+}
+
+type hasKey struct{}
+
+func (h *hasKey) Check(params []interface{}, names []string) (bool, string) {
+	if len(params) != 2 {
+		return false, "HasKey takes 2 arguments: a map and a key"
+	}
+
+	mapValue := reflect.ValueOf(params[0])
+	if mapValue.Kind() != reflect.Map {
+		return false, "first argument to HasKey must be a map"
+	}
+
+	keyValue := reflect.ValueOf(params[1])
+	if !keyValue.Type().AssignableTo(mapValue.Type().Key()) {
+		return false, "second argument must be assignable to the map key type"
+	}
+
+	return mapValue.MapIndex(keyValue).IsValid(), ""
+}
+
+func (h *hasKey) Info() *check.CheckerInfo {
+	return &check.CheckerInfo{
+		Name:   "HasKey",
+		Params: []string{"map", "key"},
+	}
 }
