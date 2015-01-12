ebb996f069c906865b02051949ad05d9c323e700
Max Persson <max@looplab.se>
2015-01-12 17:52:43 +0100

    Ignore these to make go get -t ./... run on Wercker

--- examples/mongodb/main.go
+++ examples/mongodb/main.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build mongo
+
 // Package example contains a simple runnable example of a CQRS/ES app.
 package main
 
