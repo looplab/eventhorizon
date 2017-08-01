6b1c3c00b3ef1ee919b8b4a4328d9c9c80eb59a2
Gerrit Renker <Gerrit.Renker@ctl.io>
2017-08-01 07:23:43 -0600

    address #125 review comments

--- publisher/gcp/publisher_test.go
+++ publisher/gcp/publisher_test.go
@@ -68,19 +68,18 @@ func TestEventBusAsync(t *testing.T) {
 
 // setUpEventPublisher is a helper that creates a new GCP publisher for @appID
 func setUpEventPublisher(appID string) (*EventPublisher, error) {
-	var projectID = "looplab-eventhorizon"
+	projectID := "looplab-eventhorizon"
 
 	// See https://developers.google.com/identity/protocols/application-default-credentials
 	if creds := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"); creds != "" {
-		var m = make(map[string]string)
-
 		r, err := os.Open(creds)
 		if err != nil {
 			return nil, fmt.Errorf("unable to read credentials file: %s", err)
 		}
 		defer r.Close()
 
-		if err = json.NewDecoder(r).Decode(&m); err != nil {
+		m := map[string]string{}
+		if err := json.NewDecoder(r).Decode(&m); err != nil {
 			return nil, fmt.Errorf("unable to decode %s: %s", creds, err)
 		} else if projectID = m["project_id"]; projectID == "" {
 			return nil, fmt.Errorf("project_id key not found in %s", creds)
