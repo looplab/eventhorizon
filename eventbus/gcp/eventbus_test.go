80acf3ba848a82a0d4c2fc8313575bb72fd65622
Max Ekman <max@looplab.se>
2018-09-07 16:29:56 +0200

    Add EventBus for GCP pubsub

---
+++ eventbus/gcp/eventbus_test.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2014 - The Event Horizon authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcp
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"os"
+	"testing"
+
+	"github.com/looplab/eventhorizon/eventbus"
+)
+
+func TestEventBus(t *testing.T) {
+	// Connect to localhost if not running inside docker
+	if os.Getenv("PUBSUB_EMULATOR_HOST") == "" {
+		os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8793")
+	}
+
+	// Get a random app ID.
+	b := make([]byte, 8)
+	if _, err := rand.Read(b); err != nil {
+		t.Fatal(err)
+	}
+	appID := "app-" + hex.EncodeToString(b)
+
+	bus1, err := NewEventBus("project_id", appID)
+	if err != nil {
+		t.Fatal("there should be no error:", err)
+	}
+
+	bus2, err := NewEventBus("project_id", appID)
+	if err != nil {
+		t.Fatal("there should be no error:", err)
+	}
+
+	eventbus.AcceptanceTest(t, bus1, bus2)
+
+}
