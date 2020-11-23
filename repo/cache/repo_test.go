1c10400c7c1a97daef46930d4308070003091f73
Max Ekman <max@looplab.se>
2020-11-23 09:12:45 +0100

    Fix incorrect package names for cache Repo

--- repo/cache/repo_test.go
+++ repo/cache/repo_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2014 - The Event Horizon authors.
+// Copyright (c) 2020 - The Event Horizon authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,15 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package version
+package cache
 
 import (
 	"context"
-	"reflect"
 	"testing"
-	"time"
 
-	"github.com/google/uuid"
 	eh "github.com/looplab/eventhorizon"
 	"github.com/looplab/eventhorizon/mocks"
 	"github.com/looplab/eventhorizon/repo"
@@ -43,157 +40,11 @@ func TestReadRepo(t *testing.T) {
 
 	// Read repository with default namespace.
 	repo.AcceptanceTest(t, context.Background(), r)
-	extraRepoTests(t, context.Background())
 
 	// Read repository with other namespace.
 	ctx := eh.NewContextWithNamespace(context.Background(), "ns")
 	repo.AcceptanceTest(t, ctx, r)
-	extraRepoTests(t, ctx)
 
-}
-
-func extraRepoTests(t *testing.T, ctx context.Context) {
-	simpleModel := &mocks.SimpleModel{
-		ID:      uuid.New(),
-		Content: "simpleModel",
-	}
-
-	// Cache on find.
-	baseRepo := &mocks.Repo{
-		Entity: simpleModel,
-	}
-	r := NewRepo(baseRepo)
-	entity, err := r.Find(ctx, simpleModel.ID)
-	if err != nil {
-		t.Error("there should be no error:", err)
-	}
-	if entity != simpleModel {
-		t.Error("the item should be correct")
-	}
-	if !baseRepo.FindCalled {
-		t.Error("the item should have been read from the store")
-	}
-	baseRepo.FindCalled = false
-	entity, err = r.Find(ctx, simpleModel.ID)
-	if err != nil {
-		t.Error("there should be no error:", err)
-	}
-	if entity != simpleModel {
-		t.Error("the item should be correct")
-	}
-	if baseRepo.FindCalled {
-		t.Error("the item should have been read from the cache")
-	}
-
-	// Cache on find all.
-	baseRepo = &mocks.Repo{
-		Entities: []eh.Entity{simpleModel},
-	}
-	r = NewRepo(baseRepo)
-	entities, err := r.FindAll(ctx)
-	if err != nil {
-		t.Error("there should be no error:", err)
-	}
-	if !reflect.DeepEqual(entities, []eh.Entity{simpleModel}) {
-		t.Error("the items should be correct")
-	}
-	if !baseRepo.FindAllCalled {
-		t.Error("the item should have been read from the store")
-	}
-	baseRepo.FindCalled = false
-	entity, err = r.Find(ctx, simpleModel.ID)
-	if err != nil {
-		t.Error("there should be no error:", err)
-	}
-	if entity != simpleModel {
-		t.Error("the item should be correct")
-	}
-	if baseRepo.FindCalled {
-		t.Error("the item should have been read from the cache")
-	}
-
-	// Cache bust on save.
-	baseRepo = &mocks.Repo{
-		Entity: simpleModel,
-	}
-	r = NewRepo(baseRepo)
-	entity, err = r.Find(ctx, simpleModel.ID)
-	if err != nil {
-		t.Error("there should be no error:", err)
-	}
-	if entity != simpleModel {
-		t.Error("the item should be correct")
-	}
-	if !baseRepo.FindCalled {
-		t.Error("the item should have been read from the store")
-	}
-	if err := r.Save(ctx, simpleModel); err != nil {
-		t.Error("there should be no error:", err)
-	}
-	if baseRepo.Entity != simpleModel {
-		t.Error("the item should be saved")
-	}
-	baseRepo.FindCalled = false
-	entity, err = r.Find(ctx, simpleModel.ID)
-	if err != nil {
-		t.Error("there should be no error:", err)
-	}
-	if entity != simpleModel {
-		t.Error("the item should be correct")
-	}
-	if !baseRepo.FindCalled {
-		t.Error("the item should have been read from the store")
-	}
-
-	// Cache bust on remove.
-	baseRepo = &mocks.Repo{
-		Entity: simpleModel,
-	}
-	r = NewRepo(baseRepo)
-	entity, err = r.Find(ctx, simpleModel.ID)
-	if err != nil {
-		t.Error("there should be no error:", err)
-	}
-	if entity != simpleModel {
-		t.Error("the item should be correct")
-	}
-	if !baseRepo.FindCalled {
-		t.Error("the item should have been read from the store")
-	}
-	if err := r.Remove(ctx, simpleModel.ID); err != nil {
-		t.Error("there should be no error:", err)
-	}
-	baseRepo.FindCalled = false
-	entity, err = r.Find(ctx, simpleModel.ID)
-	if err != nil {
-		t.Error("there should be no error:", err)
-	}
-	if entity != nil {
-		t.Error("the item should be correct")
-	}
-	if !baseRepo.FindCalled {
-		t.Error("the item should have been read from the store")
-	}
-
-	// Cache bust on events.
-	baseRepo = &mocks.Repo{
-		Entity: simpleModel,
-	}
-	r = NewRepo(baseRepo)
-	event := eh.NewEventForAggregate(mocks.EventType, nil,
-		time.Now(), mocks.AggregateType, simpleModel.EntityID(), 1)
-	r.HandleEvent(ctx, event)
-	baseRepo.FindCalled = false
-	entity, err = r.Find(ctx, simpleModel.ID)
-	if err != nil {
-		t.Error("there should be no error:", err)
-	}
-	if entity != simpleModel {
-		t.Error("the item should be correct")
-	}
-	if !baseRepo.FindCalled {
-		t.Error("the item should have been read from the store")
-	}
 }
 
 func TestRepository(t *testing.T) {
