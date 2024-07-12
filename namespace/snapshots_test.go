f19f019a985111c0d93bccb522be4c4eda385616
Nico <93839513+nicoandrewss@users.noreply.github.com>
2024-07-12 13:02:36 +0200

    feat: implement SnapshotStore interface in namespace eventstore

---
+++ namespace/snapshots_test.go
@@ -0,0 +1,67 @@
+// Copyright (c) 2021 - The Event Horizon authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package namespace_test
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"os"
+	"testing"
+
+	eh "github.com/Clarilab/eventhorizon"
+	"github.com/Clarilab/eventhorizon/eventstore"
+	mongodb "github.com/Clarilab/eventhorizon/eventstore/mongodb_v2"
+	"github.com/Clarilab/eventhorizon/namespace"
+)
+
+func TestEventStoreSnapshotStoreIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	// Use MongoDB in Docker with fallback to localhost.
+	addr := os.Getenv("MONGODB_ADDR")
+	if addr == "" {
+		addr = "localhost:27017"
+	}
+
+	url := "mongodb://" + addr
+
+	// Get a random DB name.
+	b := make([]byte, 4)
+	if _, err := rand.Read(b); err != nil {
+		t.Fatal(err)
+	}
+
+	db := "test-" + hex.EncodeToString(b)
+
+	t.Log("using DB:", db)
+
+	store := namespace.NewEventStore(func(ns string) (eh.EventStore, error) {
+		return mongodb.NewEventStore(url, db,
+			mongodb.WithCollectionNames(ns+"_events", ns+"_streams"),
+			mongodb.WithCollectionNames(ns+"_events", ns+"_streams"),
+		)
+	})
+
+	if store == nil {
+		t.Fatal("there should be a store")
+	}
+
+	defer store.Close()
+
+	eventstore.SnapshotAcceptanceTest(t, store, context.Background())
+}
